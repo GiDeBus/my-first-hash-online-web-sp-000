f38c7359551b6af5b443ce5c9a2c4ab465e2be9c
Ignacio Del Busto <gidelbusto@gmail.com>
2020-06-12 02:30:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,7 +5,7 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"Whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  the_manifest = {"oil paintings" => 3, "porcelain vases" => 2,"Whale bone corsets" => 5 }
 end
 
 
